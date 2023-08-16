863159988699085b9a731827b67c7f64a91ec614
João Guilherme <joaogacanuto@gmail.com>
2023-08-16 19:01:37 -0300

    Initial Commit

---
+++ AudioPlayer/AudioPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioPlayerApp.swift
+//  AudioPlayer
+//
+//  Created by Joao Guilherme Araujo Canuto on 16/08/23.
+//
+
+import SwiftUI
+
+@main
+struct AudioPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
